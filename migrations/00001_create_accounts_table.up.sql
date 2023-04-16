202c9eec80335e84c8b85bb120adbe3a0a92f878
cmyui <cmyuiosu@gmail.com>
2023-04-16 03:20:06 -0400

    fix spacing

--- migrations/00001_create_accounts_table.up.sql
+++ migrations/00001_create_accounts_table.up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE accounts (
-	account_id SERIAL PRIMARY KEY,
-	username TEXT NOT NULL,
-	email_address TEXT NOT NULL,
-	privileges INT NOT NULL,
-	password TEXT NOT NULL,
-	country TEXT NOT NULL,
-  created_at TIMESTAMPTZ NOT NULL,
-  updated_at TIMESTAMPTZ NOT NULL
+    account_id SERIAL PRIMARY KEY,
+    username TEXT NOT NULL,
+    email_address TEXT NOT NULL,
+    privileges INT NOT NULL,
+    password TEXT NOT NULL,
+    country TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL
 );
 CREATE UNIQUE INDEX ON accounts (username);
 CREATE UNIQUE INDEX ON accounts (email_address);
