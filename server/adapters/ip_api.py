63bb4c0a4c754a682fcf2e5a282675dd717cf8ba
cmyui <cmyuiosu@gmail.com>
2023-04-17 01:46:02 -0400

    clean up ip api adapter

--- server/adapters/ip_api.py
+++ server/adapters/ip_api.py
@@ -1,12 +1,16 @@
 from typing import Any
 
+from httpx import HTTPError
+
 from server import clients
 
 
 async def fetch_geolocation_from_ip_address(ip_address: str) -> dict[str, Any] | None:
     url = f"http://ip-api.com/json/{ip_address}"
-    response = await clients.http_client.get(url)
-    if response.status_code not in range(200, 300):
+    try:
+        response = await clients.http_client.get(url)
+        response.raise_for_status()
+    except HTTPError:
         return None
-
-    return response.json()
+    else:
+        return response.json()
