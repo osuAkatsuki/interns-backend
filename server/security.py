3779be2b3792cb8752cedacbfcd2e4d2b782e752
cmyui <cmyuiosu@gmail.com>
2023-04-17 21:26:00 -0400

    break up operations in hash_password for clarity

--- server/security.py
+++ server/security.py
@@ -3,10 +3,11 @@ import hashlib
 import bcrypt
 
 
-def hash_password(password: str) -> bytes:
-    return bcrypt.hashpw(
-        hashlib.md5(password.encode()).hexdigest().encode("utf-8"), bcrypt.gensalt()
-    )
+def hash_password(plaintext_password: str) -> bytes:
+    # XXX: osu! uses md5 to hash passwords; we bcrypt on top of that.
+    md5_password = hashlib.md5(plaintext_password.encode()).hexdigest()
+    bcrypt_password = bcrypt.hashpw(md5_password.encode("utf-8"), bcrypt.gensalt())
+    return bcrypt_password
 
 
 def check_password(password: str, hashword: bytes) -> bool:
