6c949572e293251db0964e6fbc9b2ea2c21d4856
cmyui <cmyuiosu@gmail.com>
2023-04-17 21:26:42 -0400

    fix minor inconsistency

--- server/security.py
+++ server/security.py
@@ -6,9 +6,9 @@ import bcrypt
 def hash_password(plaintext_password: str) -> bytes:
     # XXX: osu! uses md5 to hash passwords; we bcrypt on top of that.
     md5_password = hashlib.md5(plaintext_password.encode()).hexdigest()
-    bcrypt_password = bcrypt.hashpw(md5_password.encode("utf-8"), bcrypt.gensalt())
+    bcrypt_password = bcrypt.hashpw(md5_password.encode(), bcrypt.gensalt())
     return bcrypt_password
 
 
 def check_password(password: str, hashword: bytes) -> bool:
-    return bcrypt.checkpw(password.encode("utf-8"), hashword)
+    return bcrypt.checkpw(password.encode(), hashword)
