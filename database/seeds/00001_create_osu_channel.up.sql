517790260b3b0083b3b91cbca4ab597bf6f4dd8a
cmyui <cmyuiosu@gmail.com>
2023-04-18 22:19:44 -0400

    seed to create #osu channel

---
+++ database/seeds/00001_create_osu_channel.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO channels (name, topic, read_privileges, write_privileges,
+                      auto_join, created_at, updated_at)
+VALUES ('#osu', 'General discussion', 0, 0, TRUE, NOW(), NOW());
