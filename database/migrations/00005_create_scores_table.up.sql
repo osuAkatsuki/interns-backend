737174500396ed0f6af1addec23f2aef15b4d4c3
metal-face <captain@pirateradio.boats>
2023-04-24 00:43:49 -0400

    scores migrations written

--- database/migrations/00005_create_scores_table.up.sql
+++ database/migrations/00005_create_scores_table.up.sql
@@ -1,4 +1,5 @@
-CREATE TABLE scores (
+CREATE TABLE scores
+(
     score_id SERIAL PRIMARY KEY,
     account_id INT NOT NULL,
     online_checksum TEXT NOT NULL,
@@ -18,12 +19,20 @@ CREATE TABLE scores (
     grade TEXT NOT NULL DEFAULT 'N',
     submission_status INT NOT NULL,
     game_mode INT NOT NULL,
-    play_time datetime NOT NULL,
+    play_time TIMESTAMPTZ NOT NULL,
+    country TEXT NOT NULL,
     time_elapsed INT NOT NULL,
-    client_anticheat_flags INT NOT NULL
+    client_anticheat_flags INT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
-CREATE INDEX ON scores (account_id);
-CREATE INDEX ON scores (online_checksum);
-CREATE INDEX ON scores (beatmap_md5);
-CREATE INDEX ON scores (submission_status);
-CREATE INDEX ON scores (game_mode);
+CREATE INDEX ON scores
+(account_id);
+CREATE INDEX ON scores
+(online_checksum);
+CREATE INDEX ON scores
+(beatmap_md5);
+CREATE INDEX ON scores
+(submission_status);
+CREATE INDEX ON scores
+(game_mode);
