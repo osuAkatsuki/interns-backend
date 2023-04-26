f2ecea778fd00c2cfd7707b26b6f87ff100948dc
James-Tepper <jamesatepper@gmail.com>
2023-04-26 13:11:59 -0400

    fixed original approach to problem | created spectator repository

--- server/repositories/spectators.py
+++ server/repositories/spectators.py
@@ -1,62 +1,36 @@
-from typing import Any
+from typing import Literal
 from uuid import UUID
 
 from server import clients
 
-"""
-READ PARAMS
-account_id: id of user who's spectating
-target_id: id of user who is playing (being spectated)
-currently_spectating: True unless stopped in which case delete row
-"""
 
-READ_PARAMS = """
-    account_id
-    target_id
-    currently_spectating
-"""
+def make_key(host_session_id: int | Literal["*"]) -> str:
+    return f"server:spectators:{host_session_id}"
 
-async def create(
-    account_id: int,
-    target_id: int,
-    currently_spectating: bool,
-) -> dict[str, Any]:
-    channel = await clients.database.fetch_one(
-        query=f"""
-            INSERT INTO spectators (account_id, target_id, currently_spectating)
-            VALUES (:account_id, :target_id, :currently_spectating)
-            RETURNING {READ_PARAMS}
-        """,
-        values={
-            "account_id": account_id,
-            "target_id": target_id,
-            "currently_spectating": currently_spectating
-        }
+
+def serialize(session_id: UUID) -> str:
+    return str(session_id)
+
+
+def deserialize(channel_id: bytes) -> UUID:
+    return UUID(channel_id.decode())
+
+
+async def start_spectating(
+    host_session_id: int,
+    session_id: UUID,
+) -> UUID:
+    await clients.redis.sadd(
+        make_key(host_session_id),
+        serialize(session_id),
     )
-
-    assert channel is not None
-    return dict(channel._mapping)
+    return session_id
 
 
-async def fetch_all(
-    account_id: int,
-    target_id: int,
-    currently_spectating: bool,
-) -> list[dict[str, Any]]:
-    # TODO MAYBE FIX TRUE
-    channels = await clients.database.fetch_all(
-        query=f"""
-            SELECT {READ_PARAMS}
-            FROM spectators
-            WHERE account_id = COALESCE(:account_id, account_id)
-            AND target_id = COALESCE(:target_id, target_id)
-            AND currently_spectating = TRUE
-        """,
-        values={
-            "account_id": account_id,
-            "target_id": target_id,
-            "currently_spectating": currently_spectating
-        },
-    )
-
-    return [dict(channel._mapping) for channel in channels]
+async def stop_spectating(
+    host_session_id: int,
+    session_id: UUID,
+) -> UUID | None:
+    host_key = make_key(host_session_id)
+    success = await clients.redis.srem(host_key, serialize(session_id))
+    return session_id if success == 1 else None
