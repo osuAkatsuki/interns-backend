54184ac347ddf1d9ce0dd9a6187b380e67dc6b41
cmyui <cmyuiosu@gmail.com>
2023-04-28 23:28:44 -0400

    ass types_aiobotocore_s3 typing

--- server/clients.py
+++ server/clients.py
@@ -4,9 +4,10 @@ import aiosu
 from databases import Database
 from httpx import AsyncClient
 from redis.asyncio import Redis
+from types_aiobotocore_s3 import Client
 
 database: Database
 redis: Redis
 http_client = AsyncClient()
 osu_api: aiosu.v2.Client
-s3_client: Any
+s3_client: Client
