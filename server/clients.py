4e31690d85c1a1414f6b4bac29a61c5bb06a0ab7
metal-face <hughesbryan3000@gmail.com>
2023-04-28 22:38:39 -0400

    s3 client created

--- server/clients.py
+++ server/clients.py
@@ -2,8 +2,10 @@ import aiosu
 from databases import Database
 from httpx import AsyncClient
 from redis.asyncio import Redis
+from typing import Any
 
 database: Database
 redis: Redis
 http_client = AsyncClient()
 osu_api: aiosu.v2.Client
+s3_client: Any
