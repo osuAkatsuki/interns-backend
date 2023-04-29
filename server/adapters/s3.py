eebae5694e70b2eaaa685205a389e61c3aa20e37
metal-face <hughesbryan3000@gmail.com>
2023-04-28 22:38:30 -0400

    s3 upload and download methods written

---
+++ server/adapters/s3.py
@@ -0,0 +1,16 @@
+import settings
+import clients
+
+
+async def upload(body: bytes, filename: str, folder: str) -> None:
+    resp = await clients.s3_client.put_object(
+        Bucket=settings.AWS_S3_BUCKET_NAME, Key=f"{folder}/{filename}", Body=body
+    )
+    return None
+
+
+async def download(filename: str, folder: str) -> bytes:
+    resp = await clients.s3_client.get_object(
+        Bucket=settings.AWS_S3_BUCKET_NAME, Key=f"{folder}/{filename}"
+    )
+    return await resp["Body"].read()
