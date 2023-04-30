f67004add62e4d5abf1e7edd0b5b00e6ad144f29
cmyui <cmyuiosu@gmail.com>
2023-04-30 15:55:11 -0400

    format sql

--- database/migrations/00007_create_screenshots_table.up.sql
+++ database/migrations/00007_create_screenshots_table.up.sql
@@ -1,11 +1,9 @@
-CREATE TABLE
-    screenshots (
-        screenshot_id UUID PRIMARY KEY,
-        file_name TEXT UNIQUE NOT NULL,
-        file_type TEXT NOT NULL,
-        file_size INTEGER NOT NULL,
-        download_url TEXT NOT NULL,
-        created_at TIMESTAMPTZ DEFAULT NOW()
-    );
-
-CREATE INDEX ON screenshots(file_type);
+CREATE TABLE screenshots (
+    screenshot_id UUID PRIMARY KEY,
+    file_name TEXT UNIQUE NOT NULL,
+    file_type TEXT NOT NULL,
+    file_size INTEGER NOT NULL,
+    download_url TEXT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW()
+);
+CREATE INDEX ON screenshots (file_type);
