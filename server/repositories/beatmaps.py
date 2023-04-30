8f997c269261909044eb10aea1052880cf447d74
cmyui <cmyuiosu@gmail.com>
2023-04-30 16:01:14 -0400

    fixes for beatmaps.partial_update

--- server/repositories/beatmaps.py
+++ server/repositories/beatmaps.py
@@ -158,9 +158,8 @@ async def fetch_one_by_id(beatmap_id: int) -> Beatmap | None:
     return cast(Beatmap, beatmap) if beatmap is not None else None
 
 
-async def update_one(
-       beatmap_id: int | None = None,
-    beatmap_set_id: int | None = None,
+async def partial_update(
+    beatmap_id: int,
     ranked_status: int | None = None,
     beatmap_md5: str | None = None,
     artist: str | None = None,
@@ -185,34 +184,31 @@ async def update_one(
     beatmap = await clients.database.fetch_one(
         query=f"""\
             UPDATE beatmaps
-            SET beatmap_set_id = COALESCE(:beatmap_set_id, beatmap_set_id)
-                ranked_status = COALESCE(:ranked_status, ranked_status)
-                beatmap_md5 = COALESCE(:beatmap_md5, beatmap_md5)
-                artist = COALESCE(:artist, artist)
-                title = COALESCE(:title, title)
-                version = COALESCE(:version, version)
-                creator = COALESCE(:creator, creator)
-                filename = COALESCE(:filename, filename)
-                last_update = COALESCE(:last_update, last_update)
-                total_length = COALESCE(:total_length, total_length)
-                max_combo = COALESCE(:max_combo, max_combo)
-                ranked_status_manually_changed = COALESCE(:ranked_status_manually_changed, ranked_status_manually_changed)
-                plays = COALESCE(:plays, plays)
-                passes = COALESCE(:passes, passes)
-                mode = COALESCE(:mode, mode)
-                bpm = COALESCE(:bpm, bpm)
-                cs = COALESCE(:cs, cs)
-                ar = COALESCE(:ar, ar)
-                od = COALESCE(:od, od)
-                hp = COALESCE(:hp, hp)
+            SET ranked_status = COALESCE(:ranked_status, ranked_status),
+                beatmap_md5 = COALESCE(:beatmap_md5, beatmap_md5),
+                artist = COALESCE(:artist, artist),
+                title = COALESCE(:title, title),
+                version = COALESCE(:version, version),
+                creator = COALESCE(:creator, creator),
+                filename = COALESCE(:filename, filename),
+                last_update = COALESCE(:last_update, last_update),
+                total_length = COALESCE(:total_length, total_length),
+                max_combo = COALESCE(:max_combo, max_combo),
+                ranked_status_manually_changed = COALESCE(:ranked_status_manually_changed, ranked_status_manually_changed),
+                plays = COALESCE(:plays, plays),
+                passes = COALESCE(:passes, passes),
+                mode = COALESCE(:mode, mode),
+                bpm = COALESCE(:bpm, bpm),
+                cs = COALESCE(:cs, cs),
+                ar = COALESCE(:ar, ar),
+                od = COALESCE(:od, od),
+                hp = COALESCE(:hp, hp),
                 star_rating = COALESCE(:star_rating, star_rating)
             WHERE beatmap_id = :beatmap_id
-        
             RETURNING {READ_PARAMS}
         """,
         values={
             "beatmap_id": beatmap_id,
-            "beatmap_set_id": beatmap_set_id,
             "ranked_status": ranked_status,
             "beatmap_md5": beatmap_md5,
             "artist": artist,
