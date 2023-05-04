45cbd9110403e8e46e412fd901d5fb899ba304ef
James-Tepper <jamesatepper@gmail.com>
2023-05-04 19:56:49 -0400

    implemented fetch_many

--- server/repositories/login_attempts.py
+++ server/repositories/login_attempts.py
@@ -62,17 +62,23 @@ async def fetch_one(login_attempt_id: int) -> Login_Attempt | None:
 
 
 async def fetch_many(
-    login_attempt_id: int,
+    successful: bool | None,
+    ip_address: str | None,
+    user_agent: str | None,
     page: int | None = None,
     page_size: int | None = None,
 ) -> list[Login_Attempt] | None:
     query = f"""\
         SELECT {READ_PARAMS}
         FROM login_attempts
-        WHERE login_attempt_id = COALESCE(:login_attempt_id, login_attempt_id)
+        WHERE login_attempt_id = COALESCE(:successful, successful)
+        AND ip_address = COALESCE(:ip_address, ip_address)
+        AND user_agent = COALESCE(:user_agent user_agent)
     """
     values = {
-        "login_attempt_id": login_attempt_id,
+        "successful": successful,
+        "ip_address": ip_address,
+        "user_agent": user_agent,
     }
     if page is not None and page_size is not None:
         query += f"""\
