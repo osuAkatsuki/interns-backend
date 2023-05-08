4ddb795047d106c08fac20a33928cb93d7bcc443
cmyui <cmyuiosu@gmail.com>
2023-05-08 01:55:53 -0400

    more channels (seeds)

---
+++ database/seeds/00002_create_more_baseline_channels.down.sql
@@ -0,0 +1,5 @@
+DELETE FROM channels WHERE name = '#lobby';
+DELETE FROM channels WHERE name = '#announce';
+DELETE FROM channels WHERE name = '#help';
+DELETE FROM channels WHERE name = '#staff';
+DELETE FROM channels WHERE name = '#dev';
