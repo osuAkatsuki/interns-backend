9020cf1a8474b329785d36a0d1e74ed1d5d7a79f
cmyui <cmyuiosu@gmail.com>
2023-05-10 00:30:52 -0400

    updates to s3 adapter

--- server/adapters/s3.py
+++ server/adapters/s3.py
@@ -5,13 +5,25 @@ from server import logger
 from server import settings
 
 
-async def upload(body: bytes, filename: str, folder: str) -> None:
+async def upload(
+    body: bytes,
+    filename: str,
+    folder: str,
+    content_type: str | None = None,
+    acl: str | None = None,
+) -> None:
+    params = {
+        "Bucket": settings.S3_BUCKET_NAME,
+        "Key": f"{folder}/{filename}",
+        "Body": body,
+    }
+    if content_type is not None:
+        params["ContentType"] = content_type
+    if acl is not None:
+        params["ACL"] = acl
+
     try:
-        await clients.s3_client.put_object(
-            Bucket=settings.S3_BUCKET_NAME,
-            Key=f"{folder}/{filename}",
-            Body=body,
-        )
+        await clients.s3_client.put_object(**params)
     except botocore.exceptions.BotoCoreError as exc:
         logger.error("Failed to upload file to S3", exc_info=exc)
         return None
@@ -33,4 +45,4 @@ async def download(filename: str, folder: str) -> bytes | None:
 
 
 def get_s3_public_url(bucket_name: str, file_path: str) -> str:
-    return f"https://{bucket_name}.s3.amazonaws.com/{file_path}"
+    return f"https://s3.ca-central-1.wasabisys.com/{bucket_name}/{file_path}"
