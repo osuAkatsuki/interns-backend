92c967b32baec16298a95f11a3558cecaf0bd399
cmyui <cmyuiosu@gmail.com>
2023-05-10 00:31:33 -0400

    wokring screenshot uploads to s3

--- server/services/screenshots.py
+++ server/services/screenshots.py
@@ -24,20 +24,36 @@ async def create(
             return ServiceError.SCREENSHOTS_IMAGE_INVALID
 
     # TODO: extension name? (e.g. png, jpg)
-    screenshot_name = secrets.token_urlsafe(16)
-    screenshot_type = screenshot.format
-    assert screenshot_type is not None
+    file_name = secrets.token_urlsafe(16)
+    file_type = screenshot.format
+    assert file_type in ("PNG", "JPEG")
+
+    if file_type == "PNG":
+        file_name += ".png"
+        content_type = "image/png"
+    elif file_type == "JPEG":
+        file_name += ".jpg"
+        content_type = "image/jpeg"
+    else:
+        logger.error("Screenshot uploaded with invalid file type", file_type=file_type)
+        return ServiceError.SCREENSHOTS_IMAGE_INVALID
 
     # Upload to Amazon S3
     try:
-        await s3.upload(screenshot_data, screenshot_name, "screenshots")
+        await s3.upload(
+            screenshot_data,
+            file_name,
+            folder="screenshots",
+            content_type=content_type,
+            acl="public-read",
+        )
     except Exception as exc:  # pragma: no cover
         logger.error("Failed to upload screenshot file", exc_info=exc)
         return ServiceError.SCREENSHOTS_UPLOAD_FAILED
 
     screenshot_download_url = s3.get_s3_public_url(
         bucket_name="osu-server-professing",
-        file_path=f"screenshots/{screenshot_name}",
+        file_path=f"screenshots/{file_name}",
     )
 
     # Store screenshot metadata in database
@@ -45,8 +61,8 @@ async def create(
         screenshot_id = uuid4()
         screenshot = await screenshots.create(
             screenshot_id=screenshot_id,
-            file_name=screenshot_name,
-            file_type=screenshot_type,
+            file_name=file_name,
+            file_type=file_type,
             file_size=len(screenshot_data),
             download_url=screenshot_download_url,
         )
