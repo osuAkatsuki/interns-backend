daefaf98b939e175fa8cc0594f28a16029cf7fe1
cmyui <cmyuiosu@gmail.com>
2023-05-10 22:14:45 -0400

    add default to beatmaps updated_at

--- database/migrations/00011_add_columns_to_beatmaps_table.down.sql
+++ database/migrations/00011_add_columns_to_beatmaps_table.down.sql
@@ -1,3 +1,4 @@
 ALTER TABLE beatmaps DROP COLUMN created_at;
 ALTER TABLE beatmaps DROP COLUMN bancho_updated_at;
 ALTER TABLE beatmaps DROP COLUMN bancho_ranked_status;
+ALTER TABLE beatmaps ALTER COLUMN updated_at DROP DEFAULT;
