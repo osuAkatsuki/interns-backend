21b85ee628e76c1ffc7adc5cf8ee7b0ac7a6942c
Josh Smith <cmyuiosu@gmail.com>
2023-05-12 03:12:48 -0400

    add default to created_at in user_achievements

--- database/migrations/00012_create_user_achievements_table.up.sql
+++ database/migrations/00012_create_user_achievements_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE user_achievements (
     achievement_id INT NOT NULL,
     account_id INT NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     PRIMARY KEY (achievement_id, account_id)
 );
