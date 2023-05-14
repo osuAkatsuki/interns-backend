fadd4acab3ee67a661230b87944594f07a0dcd0d
cmyui <cmyuiosu@gmail.com>
2023-05-14 05:32:22 -0400

    fix seed #1 (channels.temporary)

--- database/seeds/00001_create_osu_channel.up.sql
+++ database/seeds/00001_create_osu_channel.up.sql
@@ -1,3 +1,3 @@
 INSERT INTO channels (name, topic, read_privileges, write_privileges,
-                      auto_join, created_at, updated_at)
-VALUES ('#osu', 'General discussion', 0, 0, TRUE, NOW(), NOW());
+                      auto_join, temporary, created_at, updated_at)
+VALUES ('#osu', 'General discussion', 0, 0, TRUE, FALSE, NOW(), NOW());
