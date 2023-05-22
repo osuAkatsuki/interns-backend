03aac5daac98023b768f7421e36c0021c200b39a
cmyui <cmyuiosu@gmail.com>
2023-05-22 14:09:36 -0400

    add v1 rest sessions to router

--- app/api/rest/__init__.py
+++ app/api/rest/__init__.py
@@ -3,7 +3,9 @@ from fastapi import APIRouter
 rest_api_router = APIRouter()
 
 from app.api.rest.v1.accounts.controllers import router as v1_accounts_router
+from app.api.rest.v1.sessions.controllers import router as v1_sessions_router
 from app.api.rest.v1.scores.controllers import router as v1_scores_router
 
 rest_api_router.include_router(v1_accounts_router)
+rest_api_router.include_router(v1_sessions_router)
 rest_api_router.include_router(v1_scores_router)
